12714025995ef9d5dcffbfeedf007f9225ca6c7d
AdToli <someEmail@>
2023-06-16 20:49:14 +0300

    actionsBLL - finish needed

--- Server/BLLs/actionsBLL.js
+++ Server/BLLs/actionsBLL.js
@@ -1,21 +1,59 @@
-
 const jFile = require("jsonfile")
 const usersBLL = require("./usersBLL")
+const actionsFile = require("../DAL/localFilePath")
 
-const getActions = async (id) => {
-    usersData = await usersBLL().getAllUsers
-    const actions = await jFile.readFile(actionsFile)
-    return actions
-}
-const getActionsById = async (id) => {
-    const actions = await jFile.readFile(actionsFile)
-    const actionArr = actions.map(el => el.id === id)
-    return actionArr;
-}
-const updateUserAction = async (actionsObj) => {
-    const actions = await getActions() // - current array of persons
-    actions.push(actionsObj)
-    return jFile.writeFile(actionsFile, actions)
+
+//GET - 
+const getAllActions = async () => {
+    const actionObj = await jFile.readFile(actionsFile)
+    const { actionsArr } = actionObj;
+    return actionsArr
 }
 
-module.exports = { getActions, getActionsById,updateUserAction }
+
+//GET - 
+const getLastUserAction = async () => {
+    const actionsArr = await getAllActions()
+    const currentUserObj = actionsArr.pop();
+    return currentUserObj
+}
+
+//GET - 
+const getAllUserActions = async () => {
+    const actionsArr = await getAllActions()
+    const { id } = await getLastUserAction();
+    const userActionsArr = actionsArr.filter(el => el.id === id)
+    return userActionsArr;
+}
+
+//GET - 
+const getActionsAllowd = async () => {
+    const { actionAllowd } = await getLastUserAction()
+    return actionAllowd;
+}
+
+//PUT -
+const updateActionsAllowd = async (event) => {
+
+//get current date
+    const dateValue = new Date();
+    const currentDate = `${dateValue.getFullYear()}-${dateValue.getMonth() + 1}-${dateValue.getDate()}`;
+
+    const actionsArr = await  getAllActions()
+    const lastUserObj = await getLastUserAction()
+    let actionAllowd = await getActionsAllowd()
+    let currentAllowd = actionAllowd - 1
+
+    const updatedActionObj = {
+        id: lastUserObj.id,
+        date: currentDate,
+        maxActions: lastUserObj.maxActions,
+        actionAllowd: currentAllowd
+    }
+    actionsArr.push(updatedActionObj)
+    jFile.writeFile(actionsFile, actionsArr)
+
+    
+}
+
+module.exports = {}
